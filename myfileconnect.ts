3e8e3da873113de15f812cdd5d18c87146f5b69d
sugam <scxetri@gmail.com>
2025-08-14 13:37:45 +0545

    Firstcommit

---
+++ myfileconnect.ts
@@ -0,0 +1,14 @@
+    //ksdfjkds
+    sdfjlsdf
+    sdflmjsdkfsd
+    FileSystemFileHandle;lsdfksdf
+    SVGDefsElement;FileSystemDirectoryEntry;FileSystemDirectoryEntrysd;FileSystemDirectoryEntry;lfsd
+    sdklfslkdf
+    sdlmf;lsdmfsd
+    screenLeft;default;sdmfdsfs\dispatchEventskdlfjl;debuggerflkghy;
+
+    fdknlghkfdg
+    WebTransportDatagramDuplexStream;mkgd
+    getSelectionfd
+    sdlkgdg
+    dfgd'
